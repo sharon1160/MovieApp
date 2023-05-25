86bf0ab4728ff396893dd18faa86a12b62a24f6d
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-25 12:58:47 -0500

    style: add open sans font

--- app/src/main/java/com/example/movieapp/view/ui/theme/Type.kt
+++ app/src/main/java/com/example/movieapp/view/ui/theme/Type.kt
@@ -1,28 +1,53 @@
 package com.example.movieapp.view.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.movieapp.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+val OpenSans = FontFamily(
+    Font(R.font.open_sans_regular, FontWeight.Normal),
+    Font(R.font.open_sans_italic, FontWeight.Light, FontStyle.Italic),
+)
+
+val typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleMedium = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = OpenSans,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontFamily = OpenSans,
+        fontSize = 10.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.15.sp,
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Light,
+        fontFamily = OpenSans,
+        fontSize = 6.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.15.sp
     )
-    */
 )
