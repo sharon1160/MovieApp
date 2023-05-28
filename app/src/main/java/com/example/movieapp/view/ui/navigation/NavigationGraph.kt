2a248d1c786071c1a7c9f95395b2568868e11aa1
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-28 13:10:30 -0500

    feat: add start destination and login view model

--- app/src/main/java/com/example/movieapp/view/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/movieapp/view/ui/navigation/NavigationGraph.kt
@@ -1,21 +1,24 @@
 package com.example.movieapp.view.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.movieapp.view.ui.LoginApplication.Companion.prefs
 import com.example.movieapp.view.ui.screens.detail.DetailScreen
 import com.example.movieapp.view.ui.screens.favorites.FavoritesScreen
 import com.example.movieapp.view.ui.screens.login.LoginScreen
 import com.example.movieapp.view.ui.screens.logout.LogoutScreen
 import com.example.movieapp.view.ui.screens.search.SearchScreen
+import com.example.movieapp.viewmodel.LoginViewModel
 
 @Composable
 fun NavigationGraph(navController: NavHostController) {
-    NavHost(navController, startDestination = NavigationItem.Search.route) {
+    val startRoute: String = getStartDestination()
+    NavHost(navController, startDestination = startRoute) {
         composable("login") {
-            LoginScreen()
+            val loginViewModel = LoginViewModel()
+            LoginScreen(loginViewModel, navController)
         }
         composable(NavigationItem.Search.route) {
             SearchScreen()
@@ -29,5 +32,13 @@ fun NavigationGraph(navController: NavHostController) {
         composable("logout") {
             LogoutScreen()
         }
+    }
+}
+
+fun getStartDestination(): String {
+    return if (prefs.isLoggedIn()){
+        "search"
+    } else {
+        "login"
     }
 }
