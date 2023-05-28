fa1b79f72bafbfb908da82b6d0eae9d8e1da75f3
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-27 20:18:10 -0500

    feat: add search screen

---
+++ app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchScreen.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.view.ui.screens.search
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.movieapp.view.ui.theme.MovieAppTheme
+
+@Composable
+fun SearchScreen() {
+    MovieAppTheme {
+        Text(text = "Helo 2")
+    }
+}
