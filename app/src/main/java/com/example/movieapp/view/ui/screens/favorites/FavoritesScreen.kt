7e704229fbbaa88887b372c7cb18bb88dcb9f4fa
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-27 20:18:19 -0500

    feat: add favorites screen

---
+++ app/src/main/java/com/example/movieapp/view/ui/screens/favorites/FavoritesScreen.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.view.ui.screens.favorites
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.movieapp.view.ui.theme.MovieAppTheme
+
+@Composable
+fun FavoritesScreen() {
+    MovieAppTheme {
+        Text(text = "Helo 3")
+    }
+}
