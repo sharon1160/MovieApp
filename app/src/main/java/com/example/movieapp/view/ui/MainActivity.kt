89e3a9993a5ca3877b285496eacb210bc2970c2d
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-27 20:16:31 -0500

    feat: add login screen

--- app/src/main/java/com/example/movieapp/view/ui/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/view/ui/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.movieapp.view.ui.screens.home.HomeScreen
+import com.example.movieapp.view.ui.screens.login.LoginScreen
 import com.example.movieapp.view.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +16,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
     }
 }
