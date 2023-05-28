45e9521b6b8724b009b9ca7db65be31b34ea136c
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-27 20:17:38 -0500

    feat: add login screen

---
+++ app/src/main/java/com/example/movieapp/view/ui/screens/login/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.view.ui.screens.login
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.movieapp.view.ui.theme.MovieAppTheme
+
+@Composable
+fun LoginScreen() {
+    MovieAppTheme {
+        Text(text = "Helo")
+    }
+}
