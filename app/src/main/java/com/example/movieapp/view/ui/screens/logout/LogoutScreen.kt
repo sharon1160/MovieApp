19d1c2e288f51f004c3da44890f78e07d8e3f292
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-27 20:17:51 -0500

    feat: add logout screen

---
+++ app/src/main/java/com/example/movieapp/view/ui/screens/logout/LogoutScreen.kt
@@ -0,0 +1,9 @@
+package com.example.movieapp.view.ui.screens.logout
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LogoutScreen() {
+    Text(text = "Logout")
+}
