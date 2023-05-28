bf4212203c1d7ac08a25041a4016a22fab0ef489
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-27 20:17:25 -0500

    feat: add detail screen

---
+++ app/src/main/java/com/example/movieapp/view/ui/screens/detail/DetailScreen.kt
@@ -0,0 +1,9 @@
+package com.example.movieapp.view.ui.screens.detail
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailScreen() {
+    Text(text = "Detail")
+}
