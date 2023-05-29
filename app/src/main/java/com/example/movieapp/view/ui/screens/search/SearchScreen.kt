7102044e622cafccbb026e50e29c5a0b691f4d40
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-29 07:51:20 -0500

    feat: remove toolbar

--- app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchScreen.kt
@@ -7,85 +7,26 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.History
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import com.example.movieapp.R
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.movieapp.view.ui.theme.MovieAppTheme
-import com.example.movieapp.view.ui.theme.OpenSans
 
 @Composable
 fun SearchScreen() {
     MovieAppTheme {
-        Scaffold(
-            topBar = { Toolbar(title = "Search", onSettingsClick = {}) },
-            content = {
-                Content(it)
-            }
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Toolbar(title: String, onSettingsClick: () -> Unit) {
-    TopAppBar(
-        title = {
-            Box(
-                modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = title,
-                    color = MaterialTheme.colorScheme.onPrimary,
-                    fontFamily = OpenSans,
-                    fontWeight = FontWeight.Bold,
-                    style = MaterialTheme.typography.titleLarge,
-                )
-            }
-        },
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primary
-        ),
-        actions = {
-            LogOutButton(
-                painter = painterResource(id = R.drawable.logout_icon),
-                description = "Logout Icon",
-                tint = MaterialTheme.colorScheme.onPrimary
-            ) {
-                onSettingsClick()
-            }
-        }
-    )
-}
-
-@Composable
-fun LogOutButton(painter: Painter, description: String, tint: Color, onClick: () -> Unit) {
-    IconButton(onClick = {
-        onClick()
-    }) {
-        Icon(
-            painter = painter,
-            contentDescription = description,
-            tint = tint,
-            modifier = Modifier
-                .width(30.dp)
-                .height(30.dp)
-        )
+        SearchContent()
     }
 }
 
 @Composable
-fun Content(paddingValues: PaddingValues) {
-    Box(modifier = Modifier
-        .padding(paddingValues)
-        .fillMaxSize()) {
+fun SearchContent() {
+    Box(
+        modifier = Modifier
+            .padding(top= 70.dp)
+            .fillMaxSize()
+    ) {
         SearchMovieBar()
     }
 }
@@ -141,7 +82,7 @@ fun SearchMovieBar() {
                 }
             }
         ) {
-            searchHistoryItems.forEach{
+            searchHistoryItems.forEach {
                 Row(modifier = Modifier.padding(all = 14.dp)) {
                     Icon(
                         modifier = Modifier.padding(end = 10.dp),
@@ -159,11 +100,6 @@ fun SearchMovieBar() {
 @Composable
 fun SearchView() {
     MovieAppTheme {
-        Scaffold(
-            topBar = { Toolbar(title = "Search", onSettingsClick = {}) },
-            content = {
-                Content(it)
-            }
-        )
+        SearchContent()
     }
 }
