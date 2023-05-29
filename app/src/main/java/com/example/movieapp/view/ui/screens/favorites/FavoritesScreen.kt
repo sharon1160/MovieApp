8c10543114e9d1f6fc8b1142e2bbe27dc34698df
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-29 07:43:46 -0500

    feat: add favorites content

--- app/src/main/java/com/example/movieapp/view/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/example/movieapp/view/ui/screens/favorites/FavoritesScreen.kt
@@ -1,12 +1,28 @@
 package com.example.movieapp.view.ui.screens.favorites
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.movieapp.view.ui.theme.MovieAppTheme
 
 @Composable
 fun FavoritesScreen() {
     MovieAppTheme {
+        FavoritesContent()
+    }
+}
+
+@Composable
+fun FavoritesContent() {
+    Box(
+        modifier = Modifier
+            .padding(top= 70.dp)
+            .fillMaxSize()
+    ) {
         Text(text = "Helo 3")
     }
 }
