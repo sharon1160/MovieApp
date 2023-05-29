f47bdc80221a01012e39c9b8bb8d07e8bac450b9
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-29 16:04:56 -0500

    feat: update validation function

--- app/src/main/java/com/example/movieapp/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/movieapp/viewmodel/LoginViewModel.kt
@@ -6,11 +6,12 @@ import com.example.movieapp.view.ui.LoginApplication.Companion.prefs
 class LoginViewModel: ViewModel() {
 
     fun validEntries(username: String, password: String): Boolean {
-        return username.isNotEmpty() && password.isNotEmpty()
+        return username == "sharon" && password == "1234"
     }
 
     fun saveSession(username: String, password: String) {
         prefs.saveUserName(username)
         prefs.savePassword(password)
+        prefs.saveIsLogged(true)
     }
 }
