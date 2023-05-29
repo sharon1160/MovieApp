af78239b2cfe359dc7d70226f0ab2d6086e68932
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-29 16:06:57 -0500

    feat: add default values for login

--- app/src/main/java/com/example/movieapp/model/Prefs.kt
+++ app/src/main/java/com/example/movieapp/model/Prefs.kt
@@ -5,9 +5,15 @@ import android.content.Context
 const val SHARED_NAME = "MyDB"
 const val SHARED_USER_NAME = "username"
 const val SHARED_PASSWORD = "password"
+const val SHARED_IS_LOGGED = "is_logged"
 
 class Prefs(context: Context) {
     private val storage = context.getSharedPreferences(SHARED_NAME, 0)
+
+    init {
+        this.saveUserName("sharon")
+        this.savePassword("1234")
+    }
 
     fun saveUserName(name: String) {
         storage.edit().putString(SHARED_USER_NAME, name).apply()
@@ -17,16 +23,20 @@ class Prefs(context: Context) {
         storage.edit().putString(SHARED_PASSWORD, password).apply()
     }
 
+    fun saveIsLogged(value: Boolean) {
+        storage.edit().putBoolean(SHARED_IS_LOGGED, value).apply()
+    }
+
     fun getUserName(): String {
         return storage.getString(SHARED_USER_NAME, "").toString()
     }
 
-    fun getPassword(): String {
-        return storage.getString(SHARED_PASSWORD, "").toString()
+    fun getIsLogged(): Boolean {
+        return storage.getBoolean(SHARED_IS_LOGGED, false)
     }
 
     fun isLoggedIn(): Boolean {
-        return getUserName().isNotEmpty() && getPassword().isNotEmpty()
+        return getIsLogged()
     }
     
     fun wipe() {
