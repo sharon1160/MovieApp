5506e64dc98c43945678bb05767884bc448e4875
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-30 12:43:23 -0500

    feat: add movies list

--- app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchScreen.kt
@@ -2,38 +2,99 @@ package com.example.movieapp.view.ui.screens.search
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.History
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.movieapp.service.model.Movie
 import com.example.movieapp.view.ui.theme.MovieAppTheme
+import com.example.movieapp.viewmodel.SearchViewModel
+
+
+val movies: MutableList<Movie> = mutableListOf(
+    Movie(
+        poster = "https://m.media-amazon.com/images/M/MV5BZDNjOGNhN2UtNmNhMC00YjU4LWEzMmUtNzRkM2RjN2RiMjc5XkEyXkFqcGdeQXVyMTU0OTM5ODc1._V1_SX300.jpg",
+        title = "Batman",
+        year = "1907"
+    ),
+    Movie(
+        poster = "https://m.media-amazon.com/images/M/MV5BZDNjOGNhN2UtNmNhMC00YjU4LWEzMmUtNzRkM2RjN2RiMjc5XkEyXkFqcGdeQXVyMTU0OTM5ODc1._V1_SX300.jpg",
+        title = "Batman",
+        year = "1907"
+    ),
+    Movie(
+        poster = "https://m.media-amazon.com/images/M/MV5BZDNjOGNhN2UtNmNhMC00YjU4LWEzMmUtNzRkM2RjN2RiMjc5XkEyXkFqcGdeQXVyMTU0OTM5ODc1._V1_SX300.jpg",
+        title = "Batman",
+        year = "1907"
+    ),
+    Movie(
+        poster = "https://m.media-amazon.com/images/M/MV5BZDNjOGNhN2UtNmNhMC00YjU4LWEzMmUtNzRkM2RjN2RiMjc5XkEyXkFqcGdeQXVyMTU0OTM5ODc1._V1_SX300.jpg",
+        title = "Batman",
+        year = "1907"
+    ),
+    Movie(
+        poster = "https://m.media-amazon.com/images/M/MV5BZDNjOGNhN2UtNmNhMC00YjU4LWEzMmUtNzRkM2RjN2RiMjc5XkEyXkFqcGdeQXVyMTU0OTM5ODc1._V1_SX300.jpg",
+        title = "Batman",
+        year = "1907"
+    ),
+    Movie(
+        poster = "https://m.media-amazon.com/images/M/MV5BZDNjOGNhN2UtNmNhMC00YjU4LWEzMmUtNzRkM2RjN2RiMjc5XkEyXkFqcGdeQXVyMTU0OTM5ODc1._V1_SX300.jpg",
+        title = "Batman",
+        year = "1907"
+    ),
+    Movie(
+        poster = "https://m.media-amazon.com/images/M/MV5BZDNjOGNhN2UtNmNhMC00YjU4LWEzMmUtNzRkM2RjN2RiMjc5XkEyXkFqcGdeQXVyMTU0OTM5ODc1._V1_SX300.jpg",
+        title = "Batman",
+        year = "1907"
+    ),
+    Movie(
+        poster = "https://m.media-amazon.com/images/M/MV5BZDNjOGNhN2UtNmNhMC00YjU4LWEzMmUtNzRkM2RjN2RiMjc5XkEyXkFqcGdeQXVyMTU0OTM5ODc1._V1_SX300.jpg",
+        title = "Batman",
+        year = "1907"
+    ),
+)
 
 @Composable
-fun SearchScreen() {
+fun SearchScreen(searchViewModel: SearchViewModel) {
+    val uiState by searchViewModel.uiState.collectAsState()
+
     MovieAppTheme {
-        SearchContent()
+        SearchContent(searchViewModel::searchByTitle, uiState.moviesList)
     }
 }
 
 @Composable
-fun SearchContent() {
+fun SearchContent(searchByTitle: (String) -> Unit, moviesList: MutableList<Movie>) {
     Box(
         modifier = Modifier
-            .padding(top= 70.dp)
+            .padding(top = 60.dp)
             .fillMaxSize()
     ) {
-        SearchMovieBar()
+        SearchMovieBar(searchByTitle)
+        if (moviesList.isNotEmpty()) {
+            MoviesList(moviesList)
+        } else {
+            Message()
+        }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchMovieBar() {
+fun SearchMovieBar(searchByTitle: (String) -> Unit) {
     var text by remember { mutableStateOf("") }
     var active by remember { mutableStateOf(false) }
     val searchHistoryItems = remember {
@@ -48,6 +109,7 @@ fun SearchMovieBar() {
             },
             onSearch = {
                 if (it.isNotEmpty()) {
+                    searchByTitle(it)
                     searchHistoryItems.add(index = 0, element = it)
                     active = false
                     text = ""
@@ -96,10 +158,117 @@ fun SearchMovieBar() {
     }
 }
 
-@Preview
+@Composable
+fun MoviesList(moviesList: List<Movie> = emptyList()) {
+    Box(modifier = Modifier.padding(top = 70.dp, bottom = 50.dp)) {
+        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+            items(items = moviesList) { movie ->
+                ListItem(movie)
+            }
+        }
+    }
+}
+
+@Composable
+fun ListItem(movie: Movie) {
+    Surface(
+        color = MaterialTheme.colorScheme.onPrimary,
+        modifier = Modifier.padding(vertical = 0.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            val lineColor = MaterialTheme.colorScheme.inversePrimary
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .drawBehind {
+                        drawLine(
+                            color = lineColor,
+                            start = Offset(0f, size.height),
+                            end = Offset(size.width, size.height),
+                            strokeWidth = 2.dp.toPx()
+                        )
+                    }
+            ) {
+
+                var moviePoster: String = movie.poster
+                if (moviePoster == "N/A") {
+                    moviePoster = "https://plantillasdememes.com/img/plantillas/imagen-no-disponible01601774755.jpg"
+                }
+
+                AsyncImage(
+                    model = moviePoster,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 6.dp, bottom = 6.dp)
+                        .height(80.dp)
+                        .width(80.dp)
+                        .clip(MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop
+                )
+
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 16.dp, end = 16.dp),
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    Text(
+                        text = movie.title,
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = movie.year,
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Light
+                    )
+                }
+                FavoritesButton()
+            }
+        }
+    }
+}
+
+@Composable
+fun FavoritesButton() {
+    val isFavorite = remember { mutableStateOf(false) }
+
+    IconButton(
+        modifier = Modifier.padding(end = 16.dp),
+        onClick = { isFavorite.value = !isFavorite.value }
+    ) {
+        val icon = if (isFavorite.value) Icons.Default.Favorite else Icons.Default.FavoriteBorder
+        val tint = if (isFavorite.value) Color.Red else Color.Gray
+        Icon(
+            imageVector = icon,
+            contentDescription = "Favorite",
+            tint = tint
+        )
+    }
+}
+
+@Composable
+fun Message() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome! Do a search")
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun SearchView() {
     MovieAppTheme {
-        SearchContent()
+        SearchContent({}, movies)
     }
 }
