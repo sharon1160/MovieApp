3c3e8bbad859e9259080aeb62181f746382bc74a
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-30 12:42:40 -0500

    feat: add search ui state

---
+++ app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchUiState.kt
@@ -0,0 +1,5 @@
+package com.example.movieapp.view.ui.screens.search
+
+import com.example.movieapp.service.model.Movie
+
+data class SearchUiState(val moviesList: MutableList<Movie> = mutableListOf())
