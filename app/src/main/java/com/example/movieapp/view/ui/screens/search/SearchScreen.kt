e72051cc907d691cf4949a28802015f01a3fa86d
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-31 17:05:51 -0500

    feat: remove unused imports

--- app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchScreen.kt
@@ -1,6 +1,5 @@
 package com.example.movieapp.view.ui.screens.search
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
