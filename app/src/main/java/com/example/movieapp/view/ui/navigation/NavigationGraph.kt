ab384a7f3f784fb55c7815d77114227938d4089a
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-31 10:52:13 -0500

    feat: add favorite movie view model

--- app/src/main/java/com/example/movieapp/view/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/movieapp/view/ui/navigation/NavigationGraph.kt
@@ -1,27 +1,37 @@
 package com.example.movieapp.view.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.room.Room
+import com.example.movieapp.service.data.database.FavoriteMovieDatabase
+import com.example.movieapp.service.repository.FavoriteMovieRepository
 import com.example.movieapp.view.ui.LoginApplication.Companion.prefs
 import com.example.movieapp.view.ui.screens.detail.DetailScreen
 import com.example.movieapp.view.ui.screens.favorites.FavoritesScreen
 import com.example.movieapp.view.ui.screens.login.LoginScreen
 import com.example.movieapp.view.ui.screens.search.SearchScreen
+import com.example.movieapp.viewmodel.FavoriteMovieViewModel
 import com.example.movieapp.viewmodel.LoginViewModel
 import com.example.movieapp.viewmodel.SearchViewModel
 
 @Composable
 fun NavigationGraph(navController: NavHostController) {
     val startRoute: String = getStartDestination()
+    val localContext = LocalContext.current
+    val db = Room.databaseBuilder(localContext, FavoriteMovieDatabase::class.java, "user_db").build()
+    val dao = db.dao
+    val repository = FavoriteMovieRepository(dao)
+    val favoriteMovieViewModel = FavoriteMovieViewModel(repository)
     NavHost(navController, startDestination = startRoute) {
         composable(NavigationItem.Search.route) {
             val searchViewModel = SearchViewModel()
-            SearchScreen(searchViewModel)
+            SearchScreen(searchViewModel, favoriteMovieViewModel)
         }
         composable(NavigationItem.Favorites.route) {
-            FavoritesScreen()
+            FavoritesScreen(favoriteMovieViewModel)
         }
         composable("detail") {
             DetailScreen()
