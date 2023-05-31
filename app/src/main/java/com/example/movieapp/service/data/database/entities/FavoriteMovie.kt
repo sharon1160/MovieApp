9534c926c642be7a97811dc29caa14f6ca943cfb
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-31 10:45:35 -0500

    feat: add favorite movie class

---
+++ app/src/main/java/com/example/movieapp/service/data/database/entities/FavoriteMovie.kt
@@ -0,0 +1,14 @@
+package com.example.movieapp.service.data.database.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorite_movie")
+data class FavoriteMovie(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") val id: Int? = null,
+    @ColumnInfo(name = "poster") val poster: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "year") val year: String,
+)
