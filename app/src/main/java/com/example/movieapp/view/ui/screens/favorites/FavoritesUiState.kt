dbe0da35db687c67a84da8efdc7443aba01b5b9b
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-02 14:55:26 -0500

    feat: add movie detail data

--- app/src/main/java/com/example/movieapp/view/ui/screens/favorites/FavoritesUiState.kt
+++ app/src/main/java/com/example/movieapp/view/ui/screens/favorites/FavoritesUiState.kt
@@ -3,5 +3,10 @@ package com.example.movieapp.view.ui.screens.favorites
 import com.example.movieapp.service.model.Movie
 
 data class FavoritesUiState(
+    val movieDetail: Movie = Movie(
+        poster = "https://plantillasdememes.com/img/plantillas/imagen-no-disponible01601774755.jpg",
+        title = "--",
+        year = "--"
+    ),
     val favoritesList: List<Movie> = emptyList()
 )
--- app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchUiState.kt
+++ app/src/main/java/com/example/movieapp/view/ui/screens/search/SearchUiState.kt
@@ -2,4 +2,11 @@ package com.example.movieapp.view.ui.screens.search
 
 import com.example.movieapp.service.model.Movie
 
-data class SearchUiState(val moviesList: MutableList<Movie> = mutableListOf())
+data class SearchUiState(
+    val moviesList: MutableList<Movie> = mutableListOf(),
+    val movieDetail: Movie = Movie(
+        poster = "https://plantillasdememes.com/img/plantillas/imagen-no-disponible01601774755.jpg",
+        title = "--",
+        year = "--"
+    )
+)
